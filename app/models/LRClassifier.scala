7771946ecf1bda5c56e3022fb17a665fa8c5d58f
Jesse Chen <jmc622@cornell.edu>
2017-03-29 14:20:13 -0400

    Fix bugs with LRClassifier, add basic tests

--- app/models/LRClassifier.scala
+++ app/models/LRClassifier.scala
@@ -7,15 +7,24 @@ import breeze.numerics._
   * Created by jessechen on 3/28/17.
   */
 class LRClassifier(dimension: Int) extends Classifier {
-  var w: DenseVector[Double] = DenseVector.zeros[Double](dimension)
+  var w: DenseVector[Double] = DenseVector.ones[Double](dimension)
 
+  /**
+    *
+    * @param xTr Training set, nxd (n is number of data points, d is dimension)
+    * @param yTr Labels corresponding to training set, 1xn
+    */
   def train(xTr: DenseMatrix[Double], yTr: DenseVector[Int]): Unit = {
-
+    adagrad(logistic, 1, 1000, 0.0001, xTr, yTr)
   }
 
-  def classify(xTe: DenseMatrix[Double]): DenseVector[Int] = {
-    new DenseVector((xTe(*, ::) dot w).toArray.map{x => sign(x)})
-  }
+  /**
+    *
+    * @param xTe Set of vectors to be classified, nxd
+    * @return Labels corresponding to xTe
+    */
+  def classify(xTe: DenseMatrix[Double]): DenseVector[Int] =
+    new DenseVector((xTe(*, ::) dot w).toArray.map(x => sign(x)))
 
   /**
     *
@@ -23,42 +32,42 @@ class LRClassifier(dimension: Int) extends Classifier {
     * @param alpha Step size
     * @param maxiter Number of iterations before automatically breaking out of loop
     * @param delta Change in w before automatically ending loop
-    * @param xTr Training set
-    * @param yTr Labels corresponding to training set
+    * @param xTr Training set, nxd
+    * @param yTr Labels corresponding to training set, 1xn
     */
-  def adagrad(lossFunc: ((DenseVector[Double], DenseMatrix[Double], DenseVector[Int]) => DenseVector[Double]),
+  def adagrad(lossFunc: ((DenseMatrix[Double], DenseVector[Int]) => DenseVector[Double]),
               alpha: Double, maxiter: Int, delta: Double,
-              xTr: DenseMatrix[Double], yTr: DenseVector[Int]): DenseVector[Double] = {
+              xTr: DenseMatrix[Double], yTr: DenseVector[Int]): Unit = {
     var z = DenseVector.zeros[Double](dimension)
     for (_ <- 1 until maxiter) {
-      var gradient = lossFunc(w, xTr, yTr)
+      val gradient = lossFunc(xTr, yTr)
       z = z + gradient.map {x => x * x}
-      val newW = w + (gradient :*= alpha) /:/ (z :+= 0.0001).map {x => Math.sqrt(x)}
+      val zEps = z :+= 0.0001
+      val alphaGradient = gradient :*= alpha
+      val newW = w + (alphaGradient) /:/ (zEps).map(x => Math.sqrt(x))
       val diff = newW + (w *:* -1.0)
-      if ((diff dot diff) < delta) return newW
+      if ((diff dot diff) < delta) return
       w = newW
     }
-    return w
   }
 
   /**
     *
-    * @param w The weight vector
-    * @param xTr Training set
-    * @param yTr Labels corresponding to training set
+    * @param xTr Training set, nxd
+    * @param yTr Labels corresponding to training set, 1xn
     * @return The gradient of the loss function at w
     */
-  def logistic(w: DenseVector[Double], xTr: DenseMatrix[Double],
+  def logistic(xTr: DenseMatrix[Double],
                yTr: DenseVector[Int]): DenseVector[Double] = {
     val n = xTr.rows
     val d = xTr.cols
 
-    val doubleYTR: DenseVector[Double] = new DenseVector(yTr.toArray.map(x => x.toDouble))
-    val YWX: DenseVector[Double] = doubleYTR *:* (xTr(*, ::) dot w)
-    var eToTheYWX: DenseVector[Double] = exp.inPlace(YWX)
-    val numerator: DenseMatrix[Double] = xTr(*, ::) *:* doubleYTR
-    val denominator: DenseVector[Double] = eToTheYWX :+= 1.0
-    val gradient = (sum(numerator /:/ denominator.t, Axis._0) :*= -1.0).t
+    val doubleYTR = new DenseVector(yTr.toArray.map(x => x.toDouble))
+    val YWX = doubleYTR *:* (xTr(*, ::) dot w)
+    var eToTheYWX = exp.inPlace(YWX)
+    val numerator = xTr(::, *) *:* doubleYTR
+    val denominator = eToTheYWX :+= 1.0
+    val gradient = (sum(numerator(::, *) /:/ denominator, Axis._0) :*= -1.0).t
 
     return gradient
   }
---
+++ test/LRClassifierSpec.scala
@@ -0,0 +1,44 @@
+import breeze.linalg.{DenseMatrix, DenseVector}
+import models.LRClassifier
+import org.scalatestplus.play.PlaySpec
+
+/**
+  * Created by jessechen on 3/29/17.
+  */
+class LRClassifierSpec extends PlaySpec {
+  "Logistic regression classifier" should {
+
+    "Train w correctly and classify correctly" in {
+      val classifier = new LRClassifier(6)
+
+      // Simple matrix to separate positive and negative vectors
+      val xTr = DenseMatrix(
+        (1.0, 2.0, 3.0, 4.0, 5.0, 6.0),
+        (1.0, 2.0, 3.9, 43.1, 2.0, 4.0),
+        (1.3, 2.4, 3.6, 4.0, 5.0, 2.0),
+        (2.1, 2.0, 3.0, 4.0, 100.0, 3.0),
+        (3.0, 2.1, 4.2, 1.5, 25.0, 3.0),
+        (-1.0, -2.0, -3.0, -4.0, -5.0, -6.0),
+        (-1.0, -2.0, -3.9, -43.1, -2.0, -4.0),
+        (-2.0, -2.4, -3.6, -4.0, -5.0, -2.0),
+        (-3.0, -2.0, -3.0, -4.0, -100.0, -3.0),
+        (-1.0, -2.1, -4.2, -1.5, -25.0, -3.0)
+      )
+
+      val yTr = DenseVector(1, 1, 1, 1, 1, -1, -1, -1, -1, -1)
+
+      val xTe = DenseMatrix(
+        (1.0, 12.5, 13.3, 15.2, 16.6, 21.1),
+        (-1.0, -5.0, -3.45, -9.1, -3.5, -0.01),
+        (0.5, 0.1, 0.3, 0.2, 0.1, 0.1)
+      )
+
+      classifier.train(xTr, yTr)
+      val y = classifier.classify(xTe).toArray
+      y.size mustBe 3
+      y(0) mustBe 1
+      y(1) mustBe -1
+      y(2) mustBe 1
+    }
+  }
+}

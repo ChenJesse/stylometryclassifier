c23eda5a3c8d5b7ffeb7959a4f2d71c8fcfaf59f
Jesse Chen <jmc622@cornell.edu>
2017-04-20 16:58:15 -0400

    Add thorough tests from ML class project 3

--- app/models/Article.scala
+++ app/models/Article.scala
@@ -1,11 +1,21 @@
 package models
 
+import breeze.linalg.DenseVector
+
 /**
   * Created by jessechen on 3/27/17.
   */
 
 class Article(text: String, date: String, source: Source) {
-  def getText = text
+  def vectorize(dimension: Int): DenseVector[Double] = {
+    val vectorArray = Array.fill[Double](dimension)(0)
+    val tokens = text.trim().split(Array('.', ' ', '!', ';', ':', '?'))
+    tokens.foreach {token =>
+      val index = Math.abs(token.hashCode()) % dimension
+      vectorArray(index) = 1.0
+    }
+    DenseVector[Double](vectorArray)
+  }
 }
 
 object Article {
--- test/LRClassifierSpec.scala
+++ test/LRClassifierSpec.scala
@@ -1,5 +1,5 @@
 import breeze.linalg.{DenseMatrix, DenseVector}
-import models.{L1Reg, L2Reg, LRClassifier, Regularization}
+import models._
 import org.scalatestplus.play.PlaySpec
 
 /**
@@ -144,6 +144,37 @@ class LRClassifierSpec extends PlaySpec {
       y(0) mustBe 1
       y(1) mustBe -1
       y(2) mustBe 1
+    }
+
+    "Train w and b correctly and classify boys/girls names" in {
+      val boysNames = scala.io.Source.fromFile("test/boys.txt").mkString.split('\n')
+      val girlsNames = scala.io.Source.fromFile("test/girls.txt").mkString.split('\n')
+
+      def nameToList(name: String): List[Double] = {
+        val vectorArray = Array.fill[Double](128)(0)
+        for (i <- 0 to 3) {
+          var featureString = "prefix" + name.slice(0, i)
+          vectorArray(Math.abs(featureString.hashCode) % 128) = 1.0
+          featureString = "suffix" + name.slice(name.length - i, name.length)
+          vectorArray(Math.abs(featureString.hashCode) % 128) = 1.0
+        }
+        vectorArray.toList
+      }
+
+      val boysVectors = boysNames.map(name => nameToList(name)).toList
+      val girlsVectors = girlsNames.map(name => nameToList(name)).toList
+      val boysLabels = List.fill[Int](boysVectors.length)(1)
+      val girlsLabels = List.fill[Int](girlsVectors.length)(-1)
+      val vectors = boysVectors ++ girlsVectors
+      val labels = boysLabels ++ girlsLabels
+      val xTr: DenseMatrix[Double] = new DenseMatrix(128, labels.length, vectors.flatten.toArray).t
+      val yTr: DenseVector[Int] = DenseVector(labels.toArray)
+
+      val classifier = new LRClassifier(128, 1, 100, 0.001, None)
+      classifier.train(xTr, yTr)
+
+      val trainingError = classifier.test(xTr, yTr)
+      assert(trainingError < 0.25)
     }
   }
 }

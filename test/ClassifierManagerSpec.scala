e419a1bcdf7cb412d6a28399dcb0afa289dff74c
Jesse Chen <jmc622@cornell.edu>
2017-05-03 00:30:21 -0400

    Test naive bayes, gets validation of 13%

--- test/ClassifierManagerSpec.scala
+++ test/ClassifierManagerSpec.scala
@@ -1,4 +1,4 @@
-import classifiers.{LinearClassifierWrapper, LogisticRegressionClassifier, Regularization}
+import classifiers.{LinearClassifierWrapper, LogisticRegressionClassifier, NaiveBayesClassifier, Regularization}
 import models.{Martin, Segment, Tolkien}
 import org.scalatestplus.play.{OneAppPerSuite, PlaySpec}
 import services.ClassifierManager
@@ -9,7 +9,7 @@ import services.ClassifierManager
 class ClassifierManagerSpec extends PlaySpec with OneAppPerSuite {
   "ClassifierManager" should {
 
-    "be able to train classifiers while interacting with MongoDB to load and store classifiers" in {
+    "be able to train logistic regression classifiers while interacting with MongoDB to load and store classifiers" in {
       val classifierManager = app.injector.instanceOf[ClassifierManager]
       val collectionName = "thirdbookvalidation"
       val classifier = new LinearClassifierWrapper[Segment](
@@ -22,9 +22,25 @@ class ClassifierManagerSpec extends PlaySpec with OneAppPerSuite {
       }
       println("Finished training!")
       val validationError = classifierManager.validate(classifier, Tolkien, Martin)
-      println(validationError)
+      println("Logistic regression validation error: " + validationError)
       assert(validationError < 0.10)
     }
 
+    "be able to train naive bayes classifiers while interacting with MongoDB to load and store classifiers" in {
+      val classifierManager = app.injector.instanceOf[ClassifierManager]
+      val collectionName = "thirdbookvalidationNB"
+      val classifier = new LinearClassifierWrapper[Segment](
+        new NaiveBayesClassifier(Segment.defaultDimension)
+      )
+
+      if (!classifierManager.loadClassifier(classifier, collectionName)) {
+        classifierManager.train(classifier, Tolkien, Martin)
+        classifierManager.persistClassifier(classifier, collectionName)
+      }
+      println("Finished training!")
+      val validationError = classifierManager.validate(classifier, Tolkien, Martin)
+      println("Naive Bayes validation error: " + validationError)
+      assert(validationError < 0.15)
+    }
   }
 }
